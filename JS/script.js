8e6655191cca31acdbf5834dbe9b8311bc969285
Sofia <srumbosnieves@gmail.com>
2022-08-28 23:29:27 -0300

    Lista de repositórios/Api

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+function validar() {
+
+    var nome = form.nome.value;
+    var email = form.email.value;
+    var telefone = form.telefone.value;
+
+ console.log(nome, email, telefone);
+
+    
+}
+console.log(validar);
+
